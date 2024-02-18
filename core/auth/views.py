bbb4e1d9ca037b50f952ba8d8b655365128b7305
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-18 16:15:45 +0330

    add test view

--- core/auth/views.py
+++ core/auth/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+class IsAuthView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        return Response({"data": "some"})
