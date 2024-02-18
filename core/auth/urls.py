9d3c846da76844aa53879ff941e33f287ca4bb1f
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-18 16:15:17 +0330

    add urls for auth apps

--- core/auth/urls.py
+++ core/auth/urls.py
@@ -3,8 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from auth.views import IsAuthView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', IsAuthView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
