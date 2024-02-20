a73b1e872ced1e762e0bb9b721201d7bd0ab73b2
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-20 16:37:14 +0330

    import refresh token serializers

--- core/auth/serializers.py
+++ core/auth/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
