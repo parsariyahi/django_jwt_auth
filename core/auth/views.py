0587dc257326689aab7de29f9b23a0184d58663a
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-24 09:52:14 +0330

    Rename and add some docstrings for views

--- core/auth/views.py
+++ core/auth/views.py
@@ -6,9 +6,17 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from auth.permissions import AdminOnlyPermission
 
-class IsAuthView(APIView):
+class TokenVerify(APIView):
+    """is Token Valid or Not
+
+    Args:
+        APIView (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
         # print(request.headers)
-        return Response({"data": "some"})
+        return Response({"vlid": True})
