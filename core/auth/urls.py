ebf31c22a2f881a0d6b46005f267edbf3cbce618
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-24 09:52:28 +0330

    Change the name of views

--- core/auth/urls.py
+++ core/auth/urls.py
@@ -3,10 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from auth.views import IsAuthView
+from auth.views import TokenVerify
 
 urlpatterns = [
-    path('', IsAuthView.as_view()),
+    path('', TokenVerify.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
