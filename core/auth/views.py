13d73f317bc93edd93adc82be7a47e6ca8909274
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-27 12:28:49 +0330

    add set cookie into views

--- core/auth/views.py
+++ core/auth/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from auth.permissions import AdminOnlyPermission
+
+
+class TokenObtain(TokenObtainPairView):
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        # print("Token Obtain")
+        response = super().post(request, *args, **kwargs)
+        ref_token = response.data["refresh"]
+        response.set_cookie("refresh", ref_token, httponly=True, secure=False)
+        # print("response cookies\n", response.cookies)
+        return response
 
 class TokenVerify(APIView):
     """is Token Valid or Not
@@ -18,5 +30,17 @@ class TokenVerify(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        # print(request.headers)
-        return Response({"vlid": True})
+        # print("Verify Token")
+        # print("headers \n", request.headers)
+        resp = Response({"vlid": True})
+        return resp
+
+class RefreshToken(TokenRefreshView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        # print("Refresh Token")
+        # print("headers \n", request.headers)
+        # print("data \n", request.data)
+        # print("cookies \n", request.COOKIES)
+        request.data["refresh"] = request.COOKIES["refresh"]
+        # print("data \n", request.data)
+        return super().post(request, *args, **kwargs)
