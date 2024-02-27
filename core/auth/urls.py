c03ee08286196b316b31b30c4ecd811998ae544d
parsariyahi <pany.parsariyahi@gmail.com>
2024-02-27 12:29:04 +0330

    add custom views into urls

--- core/auth/urls.py
+++ core/auth/urls.py
@@ -3,10 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from auth.views import TokenVerify
+from auth.views import TokenVerify, RefreshToken, TokenObtain
 
 urlpatterns = [
     path('', TokenVerify.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtain.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', RefreshToken.as_view(), name='token_refresh'),
 ]
