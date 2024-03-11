0f0a2ebac511b1b1846c0d85d54ad7d817b96f89
parsariyahi <pany.parsariyahi@gmail.com>
2024-03-11 11:46:42 +0330

    add token blacklist vew

--- core/auth/views.py
+++ core/auth/views.py
@@ -3,7 +3,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 from auth.permissions import AdminOnlyPermission
 
@@ -11,12 +11,12 @@ from auth.permissions import AdminOnlyPermission
 class TokenObtain(TokenObtainPairView):
 
     def post(self, request: Request, *args, **kwargs) -> Response:
-        # print("Token Obtain")
         response = super().post(request, *args, **kwargs)
         ref_token = response.data["refresh"]
         response.set_cookie("refresh", ref_token, httponly=True, secure=False)
-        # print("response cookies\n", response.cookies)
+
         return response
+
 
 class TokenVerify(APIView):
     """is Token Valid or Not
@@ -30,17 +30,20 @@ class TokenVerify(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        # print("Verify Token")
-        # print("headers \n", request.headers)
         resp = Response({"vlid": True})
         return resp
 
+
 class RefreshToken(TokenRefreshView):
     def post(self, request: Request, *args, **kwargs) -> Response:
-        # print("Refresh Token")
-        # print("headers \n", request.headers)
-        # print("data \n", request.data)
-        # print("cookies \n", request.COOKIES)
         request.data["refresh"] = request.COOKIES["refresh"]
-        # print("data \n", request.data)
         return super().post(request, *args, **kwargs)
+
+
+class BlacklistToken(TokenBlacklistView):
+    def post(self, request: Request, *args, **kwargs):
+        ref_token = request.COOKIES.get("refresh", None)
+        request.data["refresh"] = ref_token
+        response = super().post(request, *args, **kwargs)
+
+        return response
