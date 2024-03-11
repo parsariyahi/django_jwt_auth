46be142e2441867733f4c955db842958fe32a74b
parsariyahi <pany.parsariyahi@gmail.com>
2024-03-11 11:46:49 +0330

    add token blacklist urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -35,7 +35,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("auth.urls")),
+    path("EMS-API/auth/", include("auth.urls")),
+    path("EMS-API/user/", include("user.urls")),
 
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
